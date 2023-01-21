70f499afb0a709221efe35c877729e281b08a73f
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2023-01-21 23:36:54 +0300

    Responding to Small laptops and Landscape Tablets

--- js/script.js
+++ js/script.js
@@ -7,9 +7,51 @@ workEl.addEventListener("mouseover", function (e) {
   if (e.target.closest(".project")) {
     const project = e.target.closest(".project");
     const id = project.getAttribute("id");
-    const img = project.querySelector(".project-img");
+    const imgEl = project.querySelector(".project-img");
+    const linkEl = project.querySelector(".work-link");
 
-    img.classList.add(id);
+    imgEl.classList.add(id);
     // console.log(e.target);
   }
 });
+
+// const sectionHeroEl = document.querySelector(".section-hero");
+// const headerEl = document.querySelector(".header");
+
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
+
+//     if (ent.intersectionRatio > 0) {
+//       console.log(ent);
+//       headerEl.classList.add("sticky");
+//     } else {
+//       console.log("now");
+//       headerEl.classList.remove("sticky");
+//     }
+//   },
+//   {
+//     root: null,
+//     threshold: 0,
+//   }
+// );
+
+// obs.observe(sectionHeroEl);
+
+// const headerEl = document.querySelector(".header");
+
+// const heroSectionEl = document.querySelector(".section-hero");
+
+// const obs = new IntersectionObserver(handleIntersect);
+
+// obs.observe(heroSectionEl);
+
+// function handleIntersect(entries, observer) {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) {
+//       headerEl.classList.add("sticky");
+//     } else {
+//       headerEl.classList.remove("sticky");
+//     }
+//   });
+// }
